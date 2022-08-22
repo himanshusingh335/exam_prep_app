c54cf485777c42a1466fe2e0fe139b17d72ca63b
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2022-08-23 01:03:01 +0530

    make homescreen stateless

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,9 @@
 import 'package:exam_prep_app/widgets/grid_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key, required this.title}) : super(key: key);
+class HomeScreen extends StatelessWidget {
   final String title;
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
